8a4c69a5a900b49631cde1dac3247dcb757ae292
aarnav <aarnavbos@gmail.com>
2020-10-11 17:46:59 +0200

    add currentUser context

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,8 @@
 import Cookies from 'js-cookie'
 import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
+import { useCurrentUserQuery } from '../graphql'
+import { UserContext } from './UserContext'
 
 interface ProtectedRouteProps {
   component: React.FC
@@ -11,9 +13,23 @@ interface ProtectedRouteProps {
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ component, ...rest }) => {
   const Component = component
   if (Cookies.get('logged_in') === 'true') {
-    return <Route component={Component} {...rest} />
+    return (
+      <CurrentUserProvider>
+        <Route component={Component} {...rest} />
+      </CurrentUserProvider>
+    )
   } else {
     return <Redirect to="/login" />
   }
 }
+const CurrentUserProvider: React.FC = ({ children }) => {
+  const { data, loading, error } = useCurrentUserQuery()
+  if (!loading && !error && data) {
+    return <UserContext.Provider value={data.me ? data.me : {}}>{children}</UserContext.Provider>
+  } else {
+    // TODO: Handle exceptions
+    return <></>
+  }
+}
+
 export default ProtectedRoute
